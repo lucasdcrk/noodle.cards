479fec1d379dbf5d57fc2360654de0bb53d0f7c7
Lucas Decrock <lucas@decrock.me>
2021-03-21 01:33:40 +0100

    WIP

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,12 @@
-const abi = require('./assets/abi.json');
+const tokenAbi = require('./assets/abi/token.json');
+const farmingAbi = require('./assets/abi/farming.json');
+const swapAbi = require('./assets/abi/swap.json');
 
 module.exports = {
-    abi: abi,
-    contractAddress: '0xA9E35a238D9D7977c9c929A4e1B0127C39449d1c'
+    tokenAbi: tokenAbi,
+    tokenAddress: '0xA9E35a238D9D7977c9c929A4e1B0127C39449d1c',
+    farmingAbi: farmingAbi,
+    farmingAddress: '0xaCa2D5a061be582e9aA74caC26E6Fc0F42bbaFFD',
+    swapAbi: swapAbi,
+    swapAddress: '0x4eaAed49aE462CB1a56929106Db65EEdF61b8615'
 }
--- src/main.js
+++ src/main.js
@@ -13,8 +13,12 @@ const web3 = new web3js('https://bsc-dataseed1.binance.org:443');
 
 Vue.prototype.config = config;
 Vue.prototype.web3 = web3;
-Vue.prototype.contract = new web3.eth.Contract(config.abi, config.contractAddress);
+Vue.prototype.contract = new web3.eth.Contract(config.tokenAbi, config.tokenAddress);
+Vue.prototype.farmingContract = new web3.eth.Contract(config.farmingAbi, config.farmingAddress);
+Vue.prototype.swapContract = new web3.eth.Contract(config.swapAbi, config.swapAddress);
 Vue.prototype.ethereum = window.ethereum;
+
+window.ethereum.enable();
 
 const store = new Vuex.Store({
   state: {
